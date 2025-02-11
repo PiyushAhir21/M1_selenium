af7b83c1b180f2f472581fcea194a38809754af1
PiyushAhir21 <vasudevahir21@gmail.com>
2025-02-11 12:40:08 +0530

    xpath

--- M1_Selenium_01/src/locators/Expressions.java
+++ M1_Selenium_01/src/locators/Expressions.java
@@ -9,7 +9,7 @@ public class Expressions {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
-		driver.get("https://www.flipkart.com/");
+		driver.get("C:\\Users\\User\\Desktop\\HTML.html");
 		
 //		cssSelector
 //		driver.findElement(By.cssSelector("input[title='Search for Products, Brands and More']")).sendKeys("iphone");
@@ -17,10 +17,17 @@ public class Expressions {
 //		driver.findElement(By.cssSelector("a[class='_1TOQfO']")).click();
 		
 //		xpath
-		driver.findElement(By.xpath("//a[text()='Cart']")).click();
+//		Xpath by attribute 
+//		driver.findElement(By.xpath("//a[@class='wsejfv']")).click();
 		
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("//a[@class='wsejfv']")).click();
+//		Xpath by text()
+//		driver.findElement(By.xpath("//a[text()='Cart']")).click();
+		
+//		Xpath by contains()
+//		driver.findElement(By.xpath("//a[contains(text(),'Become a')]")).click();
+		
+//		xpath by multiple attributes
+		driver.findElement(By.xpath("//input[@type='text'and @value='A']")).sendKeys("ABCD");
 		
 		
 	}
