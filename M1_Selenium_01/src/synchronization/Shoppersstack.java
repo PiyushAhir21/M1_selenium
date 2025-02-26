a13a4a2cca38f3e135910a213fc85a8bf35ff9ad
PiyushAhir21 <vasudevahir21@gmail.com>
2025-02-26 12:31:18 +0530

    Synchronization

---
+++ M1_Selenium_01/src/synchronization/Shoppersstack.java
@@ -0,0 +1,38 @@
+package synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Shoppersstack {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		driver.get("https://www.shoppersstack.com/products_page/29");
+
+		Thread.sleep(20000);
+
+		WebElement pin = driver.findElement(By.id("Check Delivery"));
+		pin.sendKeys("201301");
+
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+//		WebElement check = wait.until(ExpectedConditions.elementToBeClickable(By.id("Check")));
+//		check.click();
+
+//		FluentWait wait = new FluentWait(driver);
+//		wait.withTimeout(Duration.ofSeconds(15));
+//		wait.until(ExpectedConditions.elementToBeClickable(By.id("Check")));
+//		wait.pollingEvery(Duration.ofSeconds(2));
+
+		driver.findElement(By.id("Check")).click();
+	}
+}
