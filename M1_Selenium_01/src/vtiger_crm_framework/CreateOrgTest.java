f5d81df1752f3a8d03940b8f9f6502beddb698ef
PiyushAhir21 <vasudevahir21@gmail.com>
2025-03-13 12:12:01 +0530

    Opportunities page

--- M1_Selenium_01/src/vtiger_crm_framework/CreateOrgTest.java
+++ M1_Selenium_01/src/vtiger_crm_framework/CreateOrgTest.java
@@ -15,43 +15,43 @@ public class CreateOrgTest {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		
+
 		driver.get("http://localhost:8888/");
-		
+
 //		Login
 		driver.get("http://localhost:8888/");
-		WebElement username =  driver.findElement(By.name("user_name"));
+		WebElement username = driver.findElement(By.name("user_name"));
 		username.sendKeys("admin");
-		WebElement password =  driver.findElement(By.name("user_password"));
+		WebElement password = driver.findElement(By.name("user_password"));
 		password.sendKeys("password");
-		WebElement submitBtn =  driver.findElement(By.id("submitButton"));
+		WebElement submitBtn = driver.findElement(By.id("submitButton"));
 		submitBtn.click();
-		
+
 		driver.findElement(By.linkText("Organizations")).click();
-		
+
 		driver.findElement(By.cssSelector("img[alt='Create Organization...']")).click();
-		
+
 		WebElement org = driver.findElement(By.name("accountname"));
-		String orgName = "jsp_" + (int)(Math.random()*1000);
+		String orgName = "jsp_" + (int) (Math.random() * 1000);
 		org.sendKeys(orgName);
-		
+
 //		save 
 		driver.findElement(By.xpath("//input[@class='crmbutton small save']")).click();
-		
+
 //		verification
 		String actOrgName = driver.findElement(By.id("dtlview_Organization Name")).getText();
 		boolean status = actOrgName.equals(orgName);
 		if (status) {
 			System.out.println("Organization created successfully");
 		}
-		
+
 //		logout
 		WebElement profile = driver.findElement(By.cssSelector("img[src=\"themes/softed/images/user.PNG\"]"));
 //		profile hover
 		Actions act = new Actions(driver);
 		act.moveToElement(profile).build().perform();
 		driver.findElement(By.linkText("Sign Out")).click();
-		
+
 //		closing browser
 		driver.close();
 	}
